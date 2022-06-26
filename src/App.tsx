a0fcaef74dfd78d4bbae45f584d3049f17b47f14
Vijay Pemmaraju <vijay@grooveapp.com>
2022-06-26 00:05:53 -0700

    Add a beat

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,58 @@
 import cx from 'classnames';
+import * as Tone from 'tone';
 import { motion } from 'framer-motion';
-import React, { FC, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 const App: FC = () => {
   const [isPlaying, setIsPlaying] = useState(false);
+  const [toneInitialized, setToneInitialized] = useState(false);
+
+  useEffect(() => {
+    if (isPlaying) {
+      if (!toneInitialized) {
+        Tone.start();
+
+        setToneInitialized(true);
+      }
+      const synth = new Tone.Synth({ volume: -10 }).toDestination();
+      const filter = new Tone.Filter(1500, 'lowpass').toDestination();
+
+      const sampler = new Tone.Sampler({
+        urls: {
+          C4: 'samples/kick.wav',
+          D4: 'samples/hat.wav',
+          E4: 'samples/rim.wav',
+        },
+        release: 1,
+        volume: -10,
+      }).toDestination();
+
+      const now = Tone.now();
+
+      // Tone.Transport.timeSignature = [4, 4];
+      console.log(Tone.Transport.timeSignature);
+      const loopC = new Tone.Loop(time => {
+        sampler.triggerAttack(['C4'], time);
+        sampler.triggerAttack(['C4'], '+0:0.5');
+        sampler.triggerAttack(['C4'], '+0:2');
+        sampler.triggerAttack(['D4'], '+0:0.25', 0.1);
+        sampler.triggerAttack(['D4'], '+0:0.5', 0.1);
+        sampler.triggerAttack(['D4'], '+0:1', 0.1);
+        sampler.triggerAttack(['E4'], '+0:1', 0.5);
+        sampler.triggerAttack(['E4'], '+0:3', 0.5);
+      }, '1m').start(0);
+      // const loopB = new Tone.Loop(time => {
+      //   sampler.triggerAttackRelease(['C4'], 4);
+      // }, '1n').start(0);
+      Tone.Transport.bpm.value = 70;
+
+      Tone.Transport.start();
+      // synth.connect(filter).triggerAttackRelease('C4', '8n');
+    } else {
+      Tone.Transport.stop();
+    }
+  }, [isPlaying]);
+
   return (
     <div className="flex flex-col items-center justify-center prose">
       <button
