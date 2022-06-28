b279471ec25a6f02993dd2ddce8a02beb51c8efb
Vijay Pemmaraju <vijay@grooveapp.com>
2022-06-27 23:04:27 -0700

    Make cool sounds

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,69 @@ import cx from 'classnames';
 import * as Tone from 'tone';
 import { motion } from 'framer-motion';
 import React, { FC, useEffect, useState } from 'react';
-import between from './between';
+import between, { betweenInt } from './between';
+
+type Chord = {
+  name: string;
+  notes: string[];
+};
+
+const melodyNotes: Chord[] = [
+  {
+    name: 'CMaj7',
+    notes: ['C4', 'D4', 'E4', 'G4'],
+  },
+  {
+    name: 'Am7',
+    notes: ['A3', 'B4', 'C4', 'E4'],
+  },
+  {
+    name: 'Em7',
+    notes: ['E3', 'G3', 'B3', 'D4'],
+  },
+  {
+    name: 'FMaj7',
+    notes: ['F3', 'A3', 'C4', 'E4'],
+  },
+];
+
+const harmonyNotes: Chord[] = [
+  {
+    name: 'CMaj7',
+    notes: ['E4', 'F4', 'G4', 'C4'],
+  },
+  {
+    name: 'Am7',
+    notes: ['C3', 'D4', 'E4', 'G4'],
+  },
+  {
+    name: 'Em7',
+    notes: ['E3', 'G3', 'B3', 'D4'],
+  },
+  {
+    name: 'FMaj7',
+    notes: ['F3', 'A3', 'C4', 'E4'],
+  },
+];
+
+const chords = [
+  {
+    name: 'CMaj7',
+    notes: ['C4', 'E4', 'G4', 'B4'],
+  },
+  {
+    name: 'Am7',
+    notes: ['A3', 'C4', 'E4', 'G4'],
+  },
+  {
+    name: 'Em7',
+    notes: ['E3', 'G3', 'B3', 'D4'],
+  },
+  {
+    name: 'FMaj7',
+    notes: ['F3', 'A3', 'C4', 'E4'],
+  },
+];
 
 const App: FC = () => {
   const [isPlaying, setIsPlaying] = useState(false);
@@ -19,8 +81,14 @@ const App: FC = () => {
 
         await Tone.loaded();
 
-        const synth = new Tone.Synth({ volume: -10 }).toDestination();
-        const filter = new Tone.Filter(2500, 'lowpass');
+        const synth = new Tone.PolySynth({
+          volume: -5,
+        }).toDestination();
+        const polysynth = new Tone.PolySynth(Tone.Synth).toDestination();
+        const filter = new Tone.Filter(1000, 'lowpass');
+        const filter2 = new Tone.Filter(1000, 'lowpass');
+        const filter3 = new Tone.Filter(1000, 'lowpass');
+        const filter4 = new Tone.Delay('8n');
 
         const sampler = await new Promise<Tone.Sampler>(resolve => {
           const s = new Tone.Sampler({
@@ -32,6 +100,7 @@ const App: FC = () => {
             release: 1,
             volume: -10,
             onload: () => {
+              s.chain(filter, filter2, filter3, filter4, Tone.Destination);
               resolve(s);
             },
           }).toDestination();
@@ -45,41 +114,111 @@ const App: FC = () => {
           ['C4'],
           ['D4'],
           ['D4', 'E4'],
-          [],
+          ['D4'],
           [],
           ['C4'],
           ['D4', 'E4'],
-          [],
+          ['D4'],
         ];
 
         const velocityMappings = {
-          C4: [0.3, 0.32],
-          D4: [0.1, 0.12],
-          E4: [0.3, 0.32],
+          C4: [0.2, 0.25],
+          D4: [0.01, 0.03],
+          E4: [0.2, 0.25],
         };
 
         let position = 0;
 
+        let currentChord = 0;
+
         Tone.Transport.scheduleRepeat(time => {
-          console.log(Tone.Transport.position);
           const delay = 0.03;
-          beat[position].forEach(sample =>
-            sampler
-              .connect(filter)
-              .triggerAttack(
-                sample,
-                time + between(0, delay),
-                between(
-                  velocityMappings[sample][0],
-                  velocityMappings[sample][1],
-                ),
+          beat[position].forEach(sample => {
+            const chosenSample = Math.random() > 0.9 ? 'D4' : sample;
+            sampler.triggerAttack(
+              chosenSample,
+              time + between(0.01, delay),
+              between(
+                velocityMappings[chosenSample][0],
+                velocityMappings[chosenSample][1],
               ),
-          );
+            );
+          });
           position += 1;
           if (position >= beat.length) {
             position = 0;
           }
         }, '8n');
+
+        let position2 = 0;
+
+        Tone.Transport.scheduleRepeat(time => {
+          const { notes } = melodyNotes[currentChord];
+          const { notes: harmony } = harmonyNotes[currentChord];
+          const delay = 0.03;
+          const noteChoice = betweenInt(0, notes.length);
+          if (Math.random() < 0.5) {
+            synth
+              .connect(filter)
+              .triggerAttackRelease(
+                notes[noteChoice],
+                '8n',
+                time + between(0.01, delay),
+                between(0.1, 0.2),
+              );
+            synth
+              .connect(filter)
+              .triggerAttackRelease(
+                harmony[noteChoice],
+                '8n',
+                time + between(0.01, delay),
+                between(0.05, 0.1),
+              );
+          }
+          position2 += 1;
+          if (position2 > 8) {
+            // currentChord = betweenInt(0, chords.length - 1);
+            currentChord += 1;
+            if (currentChord >= chords.length) {
+              currentChord = 0;
+            }
+            position2 = 1;
+          }
+        }, '4n');
+
+        const chordRhythm = [1, 1, 0, 1, 0, 1, 0, 1];
+
+        Tone.Transport.scheduleRepeat(time => {
+          const { notes } = chords[currentChord];
+          if (chordRhythm[position2 - 1] === 0) {
+            return;
+          }
+          const delay = 0.03;
+          for (const note of notes) {
+            polysynth
+              .connect(filter)
+              .triggerAttackRelease(
+                note,
+                '8n',
+                time + between(0.01, delay),
+                between(0.025, 0.01),
+              );
+          }
+        }, '4n');
+
+        const rainfall = new Tone.Player().toDestination();
+        await rainfall.load('rainfall.wav');
+        rainfall.volume.value = -20;
+        Tone.Transport.scheduleOnce(() => {
+          rainfall.connect(filter).start();
+        }, '0');
+
+        const vinyl = new Tone.Player().toDestination();
+        await vinyl.load('vinyl.flac');
+        vinyl.volume.value = -20;
+        Tone.Transport.scheduleOnce(() => {
+          vinyl.connect(filter).start();
+        }, '0');
 
         Tone.Transport.start();
       } else {
--- src/between.ts
+++ src/between.ts
@@ -2,4 +2,8 @@ function between(min: number, max: number): number {
   return Math.random() * (max - min) + min;
 }
 
+export function betweenInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 export default between;
